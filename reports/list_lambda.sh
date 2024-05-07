4941f79875a1cca78f663c016752ad8202e40818
Muhammed M.Bassem AbdElKader <56849576+muhammedabdelkader@users.noreply.github.com>
2024-05-07 09:48:04 +0200

    Update list_lambda.sh

--- reports/list_lambda.sh
+++ reports/list_lambda.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+## You Need to have AWS credentials valid
 ## Get the list of AWS regions
 regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
 accountId=$(aws sts get-caller-identity | jq '.Account')
@@ -11,4 +12,8 @@ for region in $regions; do
     echo "Listing Lambda functions in region: $region"
     aws lambda list-functions --region $region > ".lambda_${accountId}_${region}"
         done
-
+        
+## Filter Example 
+##
+# for j in $(ls ); do cd $j; echo $j; for i in $(ls -ah "." | grep ".lambda" );do cat "${i}" | jq ".Functions[]| select (.Runtime==\"go1.x\") | .FunctionName,.FunctionArn";done;cd .. ;  done
+##
